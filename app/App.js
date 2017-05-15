1af3c99c61f0682a1ccbceab9c6320b3d081d361
Nadir Özkan <nadir.ozkan@likom.com.tr>
2017-05-15 16:03:17 +0300

    Gereksiz dosyalar silindi, PigGameApp.jsx eklendi.

--- app/App.js
+++ app/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
-import {Route, Router, IndexRoute, hashHistory} from 'react-router';
-import Main from './components/Main.jsx';
+import PigGameApp from './components/PigGameApp.jsx';
 
-// Load foundation
-require('style!css!foundation-sites/dist/css/foundation.min.css')
-$(document).foundation();
-
-// Load css
-require('style!css!sass!./styles/app.scss');
 
 class App extends Component {
 
   render(){
     return (
-      <Router history = {hashHistory}>
-        <Route path="/" component={Main}>
-        </Route>
-      </Router>
+      <PigGameApp></PigGameApp>
     );
   }
 }
 
 render(<App />, document.getElementById('root'));
-
-require('./redux-example.jsx');
-// require('./redux-todo-example.jsx');
---
+++ app/components/PigGameApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PigGameApp extends React.Component {
+    render() {
+        return (
+          <div>
+            <h3>Pig Game - Redux </h3>
+          </div>
+        );
+    }
+}
+
+export default PigGameApp;
